028103fe86d2ace07d1e22efc1b3d39022c18520
qz <zqiu1307@163.com>
2021-01-18 17:06:25 +0800

    fix C3SE

--- models/yolo.py
+++ models/yolo.py
@@ -235,7 +235,7 @@ def parse_model(d, ch):  # model_dict, input_channels(3)
             #     c2 = make_divisible(c2, 8) if c2 != no else c2
 
             args = [c1, c2, *args[1:]]
-            if m in [BottleneckCSP, C3]:
+            if m in [BottleneckCSP, C3, C3SE]:
                 args.insert(2, n)
                 n = 1
         elif m is nn.BatchNorm2d:
